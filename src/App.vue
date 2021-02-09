b043b4b46a80340ba40e7a00a2ec5f0b62a2d260
Dmitriy-U <dmitriyushkalov@gmail.com>
2021-02-10 00:18:52 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <Home/>
+  </div>
+</template>
+
+<script>
+import Home from './components/Home.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Home,
+  }
+}
+</script>
+
+<style lang="scss">
+  @import '~bootstrap/scss/bootstrap.scss';
+  @import '~bootstrap-vue/src/index.scss';
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="hello">
+        <b-container>
+            <h1>Погода в Ростове-на-Дону</h1>
+            <ul>
+                <li></li>
+            </ul>
+        </b-container>
+    </div>
+</template>
+
+<script>
+    import axios from 'axios';
+
+    export default {
+        name: 'Home',
+        data: () => ({
+            current: null,
+            dayly: null,
+        }),
+        methods: {
+            async getWeather() {
+                try {
+                    const response = await axios.get('https://api.openweathermap.org/data/2.5/onecall?lat=47.2364&lon=39.7139&exclude=hourly,minutely&appid=54341f93f695264ff29b1f50899c5a8c');
+                    console.log(response);
+                    return response.data;
+                } catch (error) {
+                    console.error(error);
+                }
+            }
+        },
+
+        async mounted() {
+            const data = await this.getWeather();
+            this.current = data.current;
+            this.dayly = data.daily;
+        },
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
